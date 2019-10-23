fb9f9378f9239c8ba253ef77f22be0473ef37991
MSolomonN <54437169+MSolomonN@users.noreply.github.com>
2019-10-23 04:44:25 -0400

    Update static.py

--- static.py
+++ static.py
@@ -1,4 +1,4 @@
-def check_line(line, path):
+def check_line(line, assets_folder):
     line_list = line.split(" ") # split the line into a list
     count = 0 # save the index of an object in the list
 
@@ -8,33 +8,42 @@ def check_line(line, path):
         another_list = obj.split('"') # split the section again into a list to get the plain text inside quotes
         loc = another_list[1]
         if loc is not '#' and loc.endswith(".html") == False and loc.startswith("http") == False: # avoid # and .html
-          header = ">! static '%s%s' !<" % (path,loc)
-          header = header.replace('>', '{')
-          header = header.replace('<', '}')
+          header = "{! static '%s%s' !}" % (assets_folder,loc)
           header = header.replace('!', '%')
           another_list[1] = header
           line_list[count] = ('"').join(another_list) # after replacing content join the list
       count += 1
 
     line = (" ").join(line_list) # join the line
-    print(line.replace('/n', ''))
     return line # return edited line
 
 def main():
-  path = input("Assets folder example 'assets/' (leave blank if none): ") # static path
-  edit_file = input("Enter html file location: ") # html file location
+  assets_folder = input("> Enter assets folder. Example 'assets' (leave blank if none): ")
+  # make sure assets_folder is correct
+  if assets_folder.startswith('/'):
+    assets_folder = assets_folder.replace('/', '')     
+  if assets_folder.endswith('/') == False:
+    assets_folder = "%s/" % assets_folder
+  assets_folder.replace(' ', '') # important to remove added spaces
+
+  edit_file = input("> Enter html file location: ") # html file location
   file = open(edit_file, 'r')
 
   final_html = []
+  print(("\nPlacing {! static '%s...' !}" % assets_folder).replace('!', '%'))
   for line in file.readlines():
-    print(line)
-    l = check_line(line, path)
+    l = check_line(line, assets_folder)
     final_html.append(l) # append edited line
 
   # write a new file
-  file2 = open('new.html', 'w')
+  new_file = 'new.html'
+  file2 = open(new_file, 'w')
   new_html = ("").join(final_html)
   file2.write(new_html)
   file2.close()
+  print("\nA new file has been created '%s', copy and paste its contents." % new_file)
 
-main()
+try:
+  main()
+except:
+  print("\nAn error has occured! Make sure the html file location is correct.")
